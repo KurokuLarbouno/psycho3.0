f3040babc2c5a466a5dcba8d364de41ccedfa240
itsyoyolee <yoyo41212@gmail.com>
2018-05-03 15:44:39 +0800

    000

---
+++ scripts/player.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+var player = 0 			# 玩家編號
+var input_device = 0	# 輸入裝置編號
+var health			# 生命值
+var MOTION_SPEED	# 移動速度
+
+
+func _ready():
+
+	pass
+
+func _process(delta):
+
+	pass
