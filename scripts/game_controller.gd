f019c6f6e922b3f22e47d391f40308264308c09b
itsyoyolee <yoyo41212@gmail.com>
2018-05-17 10:41:05 +0800

    升級介面
    功能可用了，剩下把直傳回給角色&開啟下個回合

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -18,7 +18,7 @@ var rTime_total = 3
 
 var gameState = 0
 var player_data = [[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]]
-var player_stats = [[20, 6],[20, 6],[20, 6],[20, 6]] #血量、彈夾
+var player_stats = [[20, 5000, 1, 0.2, 200],[20, 5000, 1, 0.2, 200],[20, 5000, 1, 0.2, 200],[20, 5000, 1, 0.2, 200]] #血量、移動、充彈速度、攻擊速度、子彈速度
 var player_score = [[0,0],[0,0],[0,0],[0,0]] # killpoints / deadpoints
 
 func _ready():
@@ -59,7 +59,7 @@ func _process(delta):
 		#$UI/slice.start()
 		gameState = 5
 		$UI/playerUI.hide()
-		#_load_game(upgrade)
+		_load_game(upgrade)
 		if($game_scene.get_children().size() > 0):
 			$game_scene/upgrade.player_stats = player_stats
 			$game_scene/upgrade.player_score = player_score
