9d8e283f1d1a8fccb325abd338c50d4f41729d18
itsyoyolee <yoyo41212@gmail.com>
2018-05-23 19:48:59 +0800

    000

--- scripts/end_scene.gd
+++ scripts/end_scene.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
-var player_score
-var final_score
+var final_score = []
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	$Camera2D.make_current()
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	update_score()
+	pass
+
+func update_score():
+	$"1/total kills".text = str(final_score[0][0])
+	$"1/total deaths".text = str(final_score[0][1])
+	
+	pass
